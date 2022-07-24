6f44b806bfdc622f2454547a9f748ff00911657c
Mat Mark <mapkysha96@gmail.com>
2022-07-24 21:24:06 +0300

    HW 12.1 add db and ability to give away 1 item from package

--- Lesson 7/HW_7.py
+++ Lesson 7/HW_7.py
@@ -1,27 +1,40 @@
-import json
+import sqlite3
+
+connect = sqlite3.connect('d:/TMS_HW/Lesson 7/donat_items.db')
+cur = connect.cursor()
+cur.execute("""
+CREATE TABLE IF NOT EXISTS items(id INTEGER PRIMARY KEY AUTOINCREMENT,fork, amount, rule);
+            """)
+connect.commit()
+
+def tuple_converter (data):
+    return tuple(str(data).split(','))
 
 rule = input('Выберете правило раздачи вещей (LIFO или FIFO)')
 rule = rule.strip().upper()
 items_list = []
-items_list_2 = []
 while rule:
         person_item = input('Что у вас?')
         if person_item:
-            amount_preson_item = input('Сколько')
-            with open("d:/TMS_HW/Lesson 7/data.json", "w", encoding='utf-8') as file:
-                if rule=='FIFO':
-                    items_list.insert(0, {'fork': person_item, 'amount': amount_preson_item})
-                    items_list_2.insert(0, {'fork': person_item, 'amount': amount_preson_item})
-                    print('Спасибо!')
-                else:
-                    items_list.append({'fork': person_item, 'amount': amount_preson_item})
-                    items_list_2.append({'fork': person_item, 'amount': amount_preson_item})
-                    print('Спасибо!')
-                json.dump(items_list, file, ensure_ascii=False)
+            amount_preson_item = int(input('Сколько?'))
+            tuple_package_for_db = tuple_converter(person_item) + tuple_converter(amount_preson_item) + tuple_converter(rule)
+            if rule == 'FIFO':
+                items_list.insert(0, {'fork': person_item, 'amount': amount_preson_item})
+                print('Спасибо!')
+            else:
+                items_list.append({'fork': person_item, 'amount': amount_preson_item})
+                print('Спасибо!')
+            cur.execute("INSERT INTO items (fork,amount,rule) VALUES(?,?,?);", tuple_package_for_db)
+            connect.commit()
         else:
-            if len(items_list_2):
-                avaiable_item=items_list_2.pop()
-                print('Возьмите, вот вам', avaiable_item['fork'], avaiable_item['amount'], 'шт')
+            if len(items_list):
+                last_package = items_list[(len(items_list)) - 1]
+                if last_package['amount'] == 1:
+                    avaiable_package=items_list.pop()
+                    print(f"Возьмите, вот вам {avaiable_package['fork']} {avaiable_package['amount']}(шт)")
+                else:
+                    last_package['amount'] -= 1
+                    print("Возьмите вот вам:", last_package['fork'], "1(шт)")
             else:
                 print('Извните но у нас для вас пока ничего нет, попробуйте зайти позже :(')
 else:
